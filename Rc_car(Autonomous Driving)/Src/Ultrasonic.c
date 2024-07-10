8d5d37dc527690d5c8167af1631d9a7952ade6ee
Seung Jun Yang <726sj@naver.com>
2024-07-10 12:23:42 +0900

    Update Ultrasonic.c
    correction tab size

--- Rc_car(Autonomous Driving)/Src/Ultrasonic.c
+++ Rc_car(Autonomous Driving)/Src/Ultrasonic.c
@@ -26,28 +26,28 @@ uint16_t distance_F = 0;
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
 	if(htim->Instance == TIM1){
-			if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1){
-				if(captureFlag_F == 0){
-					IC_F_Value1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-					captureFlag_F = 1;
-					__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+		if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1){
+			if(captureFlag_F == 0){
+				IC_F_Value1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+				captureFlag_F = 1;
+				__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+			}
+			else if(captureFlag_F == 1){
+				IC_F_Value2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+				__HAL_TIM_SET_COUNTER(htim, 0);
+				if(IC_F_Value2 > IC_F_Value1){
+					echoTime_F = IC_F_Value2 - IC_F_Value1;
 				}
-				else if(captureFlag_F == 1){
-					IC_F_Value2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-					__HAL_TIM_SET_COUNTER(htim, 0);
-					if(IC_F_Value2 > IC_F_Value1){
-						echoTime_F = IC_F_Value2 - IC_F_Value1;
-					}
-					else if(IC_F_Value1 > IC_F_Value2){
-						echoTime_F = (0xffffffff - IC_F_Value1) + IC_F_Value2;
-					}
-					distance_F = echoTime_F / 58;
-					captureFlag_F = 0;
-					__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
-					__HAL_TIM_DISABLE_IT(htim, TIM_IT_CC1);
+				else if(IC_F_Value1 > IC_F_Value2){
+					echoTime_F = (0xffffffff - IC_F_Value1) + IC_F_Value2;
 				}
+				distance_F = echoTime_F / 58;
+				captureFlag_F = 0;
+				__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+				__HAL_TIM_DISABLE_IT(htim, TIM_IT_CC1);
 			}
 		}
+	}
 	if(htim->Instance == TIM4){
 		if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1){
 			if(captureFlag_L == 0){
@@ -72,29 +72,28 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
 		}
 	}
 	if(htim->Instance == TIM5){
-			if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1){
-				if(captureFlag_R == 0){
-					IC_R_Value1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-					captureFlag_R = 1;
-					__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+		if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1){
+			if(captureFlag_R == 0){
+				IC_R_Value1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+				captureFlag_R = 1;
+				__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+			}
+			else if(captureFlag_R == 1){
+				IC_R_Value2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+				__HAL_TIM_SET_COUNTER(htim, 0);
+				if(IC_R_Value2 > IC_R_Value1){
+					echoTime_R = IC_R_Value2 - IC_R_Value1;
 				}
-				else if(captureFlag_R == 1){
-					IC_R_Value2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-					__HAL_TIM_SET_COUNTER(htim, 0);
-					if(IC_R_Value2 > IC_R_Value1){
-						echoTime_R = IC_R_Value2 - IC_R_Value1;
-					}
-					else if(IC_R_Value1 > IC_R_Value2){
-						echoTime_R = (0xffffffff - IC_R_Value1) + IC_R_Value2;
-					}
-					distance_R = echoTime_R / 58;
-					captureFlag_R = 0;
-					__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
-					__HAL_TIM_DISABLE_IT(htim, TIM_IT_CC1);
+				else if(IC_R_Value1 > IC_R_Value2){
+					echoTime_R = (0xffffffff - IC_R_Value1) + IC_R_Value2;
 				}
+				distance_R = echoTime_R / 58;
+				captureFlag_R = 0;
+				__HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+				__HAL_TIM_DISABLE_IT(htim, TIM_IT_CC1);
 			}
 		}
-
+	}
 }
 
 void HC_SR04_1(void){
